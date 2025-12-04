6ecd746631c80ee349401c8eded95de09821e6e3
Mathias N <65249743+matnvd@users.noreply.github.com>
2025-12-03 21:58:34 -0500

    fix clustering checkstyles and commenting

--- Clustering.java
+++ Clustering.java
@@ -15,6 +15,7 @@ public class Clustering {
 
     // run the clustering algorithm and create the clusters  
     public Clustering(Point2D[] locations, int k) {
+        // initializes number of clusters and locations and throws invalid args
         if (locations == null) throw new IllegalArgumentException();
         
         this.k = k;
@@ -22,30 +23,33 @@ public class Clustering {
 
         if (k < 1 || k > m) throw new IllegalArgumentException();
 
+        // creates graph with edges between each location
         EdgeWeightedGraph graph = new EdgeWeightedGraph(m);
-        for(int i = 0; i < m; i++) {
-            for(int j = i + 1; j < m; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = i + 1; j < m; j++) {
                 graph.addEdge(new Edge(i, j, locations[i].distanceTo(locations[j])));
             }
         }
 
+        // finds MST of the graph
         KruskalMST mst = new KruskalMST(graph);
 
         Edge[] edges = new Edge[m - 1];
         int ix = 0;
-        for(Edge edge : mst.edges()) {
+        for (Edge edge : mst.edges()) {
             edges[ix] = edge;
             ix++;
         }
 
+        // creates new graph with first m - k edges
         Arrays.sort(edges);
 
-        EdgeWeightedGraph fgraph = new EdgeWeightedGraph(m);
-        for(int i = 0; i < m - k; i++) {
-            fgraph.addEdge(edges[i]);
+        EdgeWeightedGraph clusterGraph = new EdgeWeightedGraph(m);
+        for (int i = 0; i < m - k; i++) {
+            clusterGraph.addEdge(edges[i]);
         }
-
-        components = new CC(fgraph);
+    
+        components = new CC(clusterGraph);
     }
 
     // return the cluster of the ith location 
@@ -59,8 +63,9 @@ public class Clustering {
     public int[] reduceDimensions(int[] input) {
         if (input == null) throw new IllegalArgumentException();
 
+        // add each input to their respective components index in the out[]
         int[] out = new int[k];
-        for(int i = 0; i < input.length; i++) {
+        for (int i = 0; i < input.length; i++) {
             out[clusterOf(i)] += input[i];
         }
 
@@ -69,12 +74,15 @@ public class Clustering {
 
     // unit testing (required)
     public static void main(String[] args) {
+        // c: number of centers, p: number of points per cluster
         int c = Integer.parseInt(args[0]);
         int p = Integer.parseInt(args[1]);
 
+        // stores centers for which points will be clustered around
         Point2D[] centers = new Point2D[c];
-        for(int i = 0; i < c; i++) {
-            if(i == 0) {
+        for (int i = 0; i < c; i++) {
+            // generates a random point, making sure its >= 4 away from other centers
+            if (i == 0) {
                 double randX = StdRandom.uniformDouble(0.0, 1000.0);
                 double randY = StdRandom.uniformDouble(0.0, 1000.0);
                 Point2D randPt = new Point2D(randX, randY);
@@ -87,22 +95,28 @@ public class Clustering {
                     Point2D randPt = new Point2D(randX, randY);
                     centers[i] = randPt;
 
+                    // makes sure its far enough away from all other centers
                     minDist = Double.MAX_VALUE;
-                    for(int j = 0; j < i; j++) {
+                    for (int j = 0; j < i; j++) {
                         double dist = centers[i].distanceTo(centers[j]);
-                        if(dist < minDist) minDist = dist;
+                        if (dist < minDist) minDist = dist;
                     }
                 }
             }
         }
 
+        // stores locations of all points
         Point2D[] locations = new Point2D[c * p];
-        for(int i = 0; i < c; i++) {
+        for (int i = 0; i < c; i++) {
             for (int j = 0; j < p; j++) {
                 double dist = 2;
                 while (dist > 1) {
-                    double randX = StdRandom.uniformDouble(centers[i].x() - 1, centers[i].x() + 1);
-                    double randY = StdRandom.uniformDouble(centers[i].y() - 1, centers[i].y() + 1);
+                    // generates a random point +- 1 unit away from the center
+                    // calculates distance from center, if too far, generates again
+                    double cx = centers[i].x();
+                    double cy = centers[i].y();
+                    double randX = StdRandom.uniformDouble(cx - 1, cx + 1);
+                    double randY = StdRandom.uniformDouble(cy - 1, cy + 1);
                     Point2D randPt = new Point2D(randX, randY);
                     dist = randPt.distanceTo(centers[i]);
                     locations[i * p + j] = randPt;
@@ -111,10 +125,10 @@ public class Clustering {
         }
 
         Clustering clusterTest = new Clustering(locations, c);
-        for(int i = 0; i < c * p; i++) {
+        for (int i = 0; i < c * p; i++) {
             // compares each location in cluster to the first location in the cluster
-            int firstLocInCluster = i - i % p;
-            if (clusterTest.clusterOf(i) != clusterTest.clusterOf(firstLocInCluster)) {
+            int firstInCluster = i - i % p;
+            if (clusterTest.clusterOf(i) != clusterTest.clusterOf(firstInCluster)) {
                 System.out.println("Error!");
             }
         }
