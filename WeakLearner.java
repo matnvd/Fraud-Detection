f2a0e352ca3d39b711a805b0d400b69f57b3bd38
VisH317 <vt201916384@gmail.com>
2025-12-06 15:56:08 -0500

    added comments

--- WeakLearner.java
+++ WeakLearner.java
@@ -16,6 +16,7 @@ public class WeakLearner {
             this.ix = ix;
         }
         
+        // compares with specific index
         public int compare(int[] arr1, int[] arr2) {
             return Integer.compare(arr1[ix], arr2[ix]);
         }
@@ -38,9 +39,11 @@ public class WeakLearner {
         // num of clusters (each like a point of k dim)
         k = input[0].length;
 
-        // input that we can re-sort
+        // copy of original input with extra column to keep original index
+        // preserves index after in place sort Arrays.sort
         int[][] sortableInput = new int[n][k + 1];
 
+        // calculates sum of all weights
         double totalWeight = 0;
 
         for (int i = 0; i < n; i++) {
@@ -50,6 +53,7 @@ public class WeakLearner {
                 throw new IllegalArgumentException();
             }
 
+            // populate sortable input
             for (int j = 0; j < k; j++) {
                 sortableInput[i][j] = input[i][j];
             }
@@ -59,42 +63,53 @@ public class WeakLearner {
             totalWeight += weights[i];
         }
 
-        // calculates default weight as the middle weight
+        // holds the 50% weight value, used to compute opposite sign value
+        // score - midWeight returns max score for opposite sign
         double midWeight = totalWeight / 2;
 
         double maxScore = midWeight;
 
-        // goes through and tries partitioning by each dimension
+        // iterate through each dimension
         for (int dim = 0; dim < k; dim++) {
+            // sort by the current dimension
             Arrays.sort(sortableInput, new DatasetComparator(dim));
 
-            // calculates the default score for it the line was at the start
+            // calculates the default score (assume 1 predicted for all items)
             double defaultScore = 0;
             for (int i = 0; i < n; i++) {
                 int accIdx = sortableInput[i][k];
                 if (labels[accIdx] == 1) defaultScore += weights[accIdx];
             }
 
-            // iterates through each point and calculates a new score for if there
-            // was a partition through that point at the current dimension
+            // initial score to update by adding/removing weight from each point
             double curScore = defaultScore;
-
+            
+            // iterate through each point in ascending order by dimension
             for (int i = 0; i < n; i++) {
-                // new point setting partition
+                // stores original index of the data point
                 int accIdx = sortableInput[i][k];
                 
-                // ADD COMMENTS
-                if (labels[accIdx] == 0) curScore += weights[accIdx];
-                else curScore -= weights[accIdx];
+                // now, we move the partition vp to this point
+                // since one point gets moved to the other side, we calculate new 
+                // score by either adding or subracting from the previous score
 
+                // current point switch to lower side, if value matches, add weight
+                if (labels[accIdx] == 0) curScore += weights[accIdx]; 
+                else curScore -= weights[accIdx]; // if point is now wrong, subtract
+
+                // if next point has same value, do not evaluate score
+                // this is needed bc not all the points are shifted with duplicates
                 if (i < n - 1 && sortableInput[i + 1][dim] == sortableInput[i][dim]) {
                     continue;
                 }
                 
+                // determine distance from 50% randomness (determines value)
                 double newMax = midWeight + Math.abs(curScore - midWeight);
+                // update to new max if is greater
                 if (newMax > maxScore) {
                     dp = dim;
                     vp = sortableInput[i][dim];
+                    // if it is below 50%, switch sign to increase accuracy
                     if (curScore < defaultScore) {
                         sp = 1;
                     } else {
@@ -110,7 +125,7 @@ public class WeakLearner {
     public int predict(int[] sample) {
         if (sample == null) throw new IllegalArgumentException();
         if (sample.length != k) throw new IllegalArgumentException();
-
+        
         if (sp == 1) {
             if (sample[dp] <= vp) {
                 return 1;
